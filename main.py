0ea380a6c42fd78aa41425ac3f115db02611d0d5
Lia Porto <liabarcellos28@gmail.com>
2021-08-10 10:31:28 -0300

    test: change menu saving condition to true for testing

--- main.py
+++ main.py
@@ -174,7 +174,7 @@ def getCardapioCampus(keyCampus):
 
 
     #guarda o cardápio da semana em um csv separado caso seja segunda-feira.
-    if diaDaSemana == "Segunda-Feira":
+    if True:
         try:
             relative_path = f"/data/cardapiomes{month}-{year}-{campusArqName}.csv"
             path = os.path.abspath(os.path.dirname(__file__)) + relative_path
