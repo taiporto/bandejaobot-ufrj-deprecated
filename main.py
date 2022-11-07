77941b24588528ae41060e8e0ace8244b369bcf6
Moon Koala <moonkoala23@gmail.com>
2022-11-07 09:00:34 -0300

    fix: remove profile handle from update_status call

--- main.py
+++ main.py
@@ -246,8 +246,7 @@ def postTweets(stringArray):
         if len(string) >= 204:
             newTweets = splitTweet(string)
             firstTweet = api.update_status(newTweets[0])
-            secondTweet = f'@bandejaobotufrj{newTweets[1]}'
-            api.update_status(secondTweet, firstTweet.id_str)
+            api.update_status(newTweets[1], firstTweet.id_str)
         else:
             api.update_status(string)
 
