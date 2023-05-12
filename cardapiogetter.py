ba374a490096f33fa97b80899fdf150ea329a141
taiporto <tai.dporto@gmail.com>
2023-05-12 17:34:00 +0000

    fix: change html parsing method and week day finding

--- cardapiogetter.py
+++ cardapiogetter.py
@@ -5,43 +5,43 @@ import pandas as pd
 
 def getCardapio(url):
 
-    data = []
+  data = []
 
-    html = urlopen(url)
-    res = soup(html.read(),"html5lib")
+  html = urlopen(url)
+  res = soup(html.read(), "html.parser")
 
-    tbody = res.find('tbody')
+  tbody = res.find('tbody')
 
-    tableRows = tbody.find_all('tr')
-    tableRows.pop(0)
+  tableRows = tbody.find_all('tr')
+  tableRows.pop(0)
 
-    for row in tableRows:
-        columns = row.find_all('td')
-        if row:
-            output_row = []
-            for column in columns:
-                    output_row.append(column.text)
-            data.append(output_row)
+  for row in tableRows:
+    columns = row.find_all('td')
+    if row:
+      output_row = []
+      for column in columns:
+        output_row.append(column.text)
+      data.append(output_row)
 
-    df = pd.DataFrame(data)
-    dfLunch = df.iloc[0:8]
-    dfLunch.reset_index(drop=True)
+  df = pd.DataFrame(data)
+  dfLunch = df.iloc[0:8]
+  dfLunch.reset_index(drop=True)
 
-    dfDinner = df.iloc[8:16]
+  dfDinner = df.iloc[8:16]
 
-    new_header = dfLunch.iloc[0]
-    new_header[4] = "Quinta-Feira"
-    dfLunch = dfLunch[1:]
-    dfLunch.columns = new_header
-    new_header = dfDinner.iloc[0]
-    new_header[4] = "Quinta-Feira"
-    dfDinner = dfDinner[1:]
-    dfDinner.columns = new_header
+  new_header = dfLunch.iloc[0]
+  new_header[4] = "Quinta-Feira"
+  dfLunch = dfLunch[1:]
+  dfLunch.columns = new_header
+  new_header = dfDinner.iloc[0]
+  new_header[4] = "Quinta-Feira"
+  dfDinner = dfDinner[1:]
+  dfDinner.columns = new_header
 
-    if (dfLunch is not None) and (dfDinner is not None):
-        print("Resultado pronto!")
-        print(url)
-        print(dfLunch, dfDinner)
-        return [dfLunch, dfDinner]
-    else:
-        print("Erro")
+  if (dfLunch is not None) and (dfDinner is not None):
+    print("Resultado pronto!")
+    print(url)
+    print(dfLunch, dfDinner)
+    return [dfLunch, dfDinner]
+  else:
+    print("Erro")
